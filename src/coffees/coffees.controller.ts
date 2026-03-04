bdd516c695c2120a27cd2c6b3fe0f3e95c60f2b0
P Gomes <pritgomes@gmail.com>
2026-03-04 14:11:33 +1000

    chore: comment out entity

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,14 @@
 import { CircuitBreakerInterceptor } from '@/common/interceptors/circuit-breaker/circuit-breaker.interceptor';
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  RequestTimeoutException,
-  UseInterceptors,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+	RequestTimeoutException,
+	UseInterceptors,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -17,31 +17,35 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 @UseInterceptors(CircuitBreakerInterceptor)
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {}
+	constructor(private readonly coffeesService: CoffeesService) {}
 
-  @Post()
-  create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    return this.coffeesService.create(createCoffeeDto);
-  }
+	@Post()
+	create(@Body() createCoffeeDto: CreateCoffeeDto) {
+		return this.coffeesService.create(createCoffeeDto);
+	}
 
-  @Get()
-  findAll() {
-    console.log('🦊 "findAll" executed');
-    throw new RequestTimeoutException('💥 Error!');
-  }
+	@Get()
+	findAll() {
+		console.log('🦊 "findAll" executed');
+		throw new RequestTimeoutException('💥 Error!');
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(+id);
-  }
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.coffeesService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
-    return this.coffeesService.update(+id, updateCoffeeDto);
-  }
+	@Patch(':id')
+	update(
+		@Param('id') id: string,
+		// @Param('id', EntityExistsPipe(Coffee)) id: string,
+		@Body() updateCoffeeDto: UpdateCoffeeDto,
+	) {
+		return this.coffeesService.update(+id, updateCoffeeDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.coffeesService.remove(+id);
-  }
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.coffeesService.remove(+id);
+	}
 }
