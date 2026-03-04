9bfffaebeaf931f7c87f9948bebffb98d034c072
P Gomes <pritgomes@gmail.com>
2026-03-04 13:50:23 +1000

    feat: add http client module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CronModule } from './cron/cron.module';
-import { SchedulerModule } from './scheduler/scheduler.module';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
+import { HttpClientModule } from './http-client/http-client.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
-  imports: [CoffeesModule, CronModule, SchedulerModule, FibonacciModule],
+  imports: [
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    CoffeesModule,
+    CronModule,
+    SchedulerModule,
+    FibonacciModule,
+    HttpClientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
