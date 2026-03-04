d1941b6133a1279cdae5c1cb1e227f78db7e8cb3
P Gomes <pritgomes@gmail.com>
2026-03-04 12:50:03 +1000

    feat: added fibobacci worker threads

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CronModule } from './cron/cron.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { FibonacciModule } from './fibonacci/fibonacci.module';
 
 @Module({
-  imports: [CoffeesModule, CronModule, SchedulerModule],
+  imports: [CoffeesModule, CronModule, SchedulerModule, FibonacciModule],
   controllers: [AppController],
   providers: [AppService],
 })
